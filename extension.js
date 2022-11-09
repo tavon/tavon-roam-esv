ca79fbec02493f550226d050f2190d91d3d55c8d
John Hwang <johnyhwang@gmail.com>
2022-11-09 00:22:28 -0500

    Refactor code and clean up output

--- extension.js
+++ extension.js
@@ -1,280 +1,39 @@
-const API_URL = `https://api.esv.org/v3/passage/text/`;
-
-// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/writes/createBlock.ts
-const createBlock = (params) => {
-  const uid = window.roamAlphaAPI.util.generateUID();
-  return Promise.all(
-    [
-      window.roamAlphaAPI.createBlock({
-        location: {
-          "parent-uid": params.parentUid,
-          order: params.order,
-        },
-        block: {
-          uid,
-          string: params.node.text,
-        },
-      }),
-    ].concat(
-      (params.node.children || []).map((node, order) =>
-        createBlock({ parentUid: uid, order, node })
-      )
-    )
-  );
-};
-
-// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/components/FormDialog.tsx
-const FormDialog = ({ onSubmit, title, options, question, onClose }) => {
-  const [data, setData] = window.React.useState(options[0].id);
-  const onClick = window.React.useCallback(() => {
-    onSubmit(data);
-    onClose();
-  }, [data, onClose]);
-  const onCancel = window.React.useCallback(() => {
-    onSubmit("");
-    onClose();
-  }, [onClose]);
-  return window.React.createElement(
-    window.Blueprint.Core.Dialog,
-    { isOpen: true, onClose: onCancel, title },
-    window.React.createElement(
-      "div",
-      { className: window.Blueprint.Core.Classes.DIALOG_BODY },
-      question,
-      window.React.createElement(
-        window.Blueprint.Core.Label,
-        {},
-        "ESV Bible Query:",
-        window.React.createElement(
-          window.Blueprint.Select.Select,
-          {
-            activeItem: data,
-            onItemSelect: (id) => setData(id),
-            items: options.map((opt) => opt.id),
-            itemRenderer: (item, { modifiers, handleClick }) =>
-              window.React.createElement(window.Blueprint.Core.MenuItem, {
-                key: item,
-                text: options.find((opt) => opt.id === item).label,
-                active: modifiers.active,
-                onClick: handleClick,
-              }),
-            filterable: false,
-            popoverProps: {
-              minimal: true,
-              captureDismiss: true,
-            },
-          },
-          window.React.createElement(window.Blueprint.Core.Button, {
-            text: options.find((opt) => opt.id === data).label,
-            rightIcon: "double-caret-vertical",
-          })
-        )
-      )
-    ),
-    window.React.createElement(
-      "div",
-      { className: window.Blueprint.Core.Classes.DIALOG_FOOTER },
-      window.React.createElement(
-        "div",
-        { className: window.Blueprint.Core.Classes.DIALOG_FOOTER_ACTIONS },
-        window.React.createElement(window.Blueprint.Core.Button, {
-          text: "Cancel",
-          onClick: onCancel,
-        }),
-        window.React.createElement(window.Blueprint.Core.Button, {
-          text: "Submit",
-          intent: "primary",
-          onClick,
-        })
-      )
-    )
-  );
-};
-
-const prompt = ({ options, question, title }) =>
-  new Promise((resolve) => {
-    const app = document.getElementById("app");
-    const parent = document.createElement("div");
-    parent.id = "esv-prompt-root";
-    app.parentElement.appendChild(parent);
-
-    window.ReactDOM.render(
-      window.React.createElement(FormDialog, {
-        onSubmit: resolve,
-        title,
-        options,
-        question,
-        onClose: () => {
-          window.ReactDOM.unmountComponentAtNode(parent);
-          parent.remove();
-        },
-      }),
-      parent
-    );
-  });
-
-const config = {
-  tabTitle: "ESV Bible",
-  settings: [
-    {
-      id: "esv-org-api-key",
-      name: "ESV.org API Token",
-      description: "Generated API Token from ESV.org",
-      action: {
-        type: "input",
-      },
-    },
-  ],
-};
-
 export default {
   onload: ({ extensionAPI }) => {
+    // console.log("onload");
+
+    // Extension Settings - You need an API token from ESV.org
+    const config = {
+      tabTitle: "ESV Bible",
+      settings: [
+        {
+          id: "esv-org-api-key",
+          name: "ESV.org API Token",
+          description: "Generated API Token from ESV.org",
+          action: {
+            type: "input",
+          },
+        },
+      ],
+    };
+
     extensionAPI.settings.panel.create(config);
+
+    let token;
+    if (!extensionAPI.settings.get("esv-org-api-key")) {
+      alert(
+        "Please enter your API key in the configuration settings via the Roam Depot tab."
+      );
+      return;
+    } else {
+      token = extensionAPI.settings.get("esv-org-api-key");
+      // console.log("token: " + token);
+    }
 
     window.roamAlphaAPI.ui.commandPalette.addCommand({
       label: "ESV Bible",
-      callback: () => {
-        const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
-
-        if (uid == undefined) {
-          alert("Please make sure to focus a block before searching ESV");
-          return;
-        }
-
-        fetchESV(uid).then(async (blocks) => {
-          await window.roamAlphaAPI.updateBlock({
-            block: { uid: uid, string: blocks[0].text.toString(), open: true },
-          });
-
-          for (var i = 0; i < blocks[0].children.length; i++) {
-            var thisBlock = window.roamAlphaAPI.util.generateUID();
-            await window.roamAlphaAPI.createBlock({
-              location: { "parent-uid": uid, order: i + 1 },
-              block: {
-                string: blocks[0].children[i].text.toString(),
-                uid: thisBlock,
-              },
-            });
-          }
-          const pageId = await window.roamAlphaAPI.pull("[*]", [
-            ":block/uid",
-            uid,
-          ])?.[":block/page"]?.[":db/id"];
-          const pageUID = await window.roamAlphaAPI.pull(
-            "[:block/uid]",
-            pageId
-          )?.[":block/uid"];
-          let order = await window.roamAlphaAPI.q(
-            `[:find ?o :where [?r :block/order ?o] [?r :block/uid "${uid}"]]`
-          )?.[0]?.[0]; // thanks to David Vargas https://github.com/dvargas92495/roam-client/blob/main/src/queries.ts#L58
-          var thisBlock = window.roamAlphaAPI.util.generateUID();
-
-          await window.roamAlphaAPI.createBlock({
-            location: { "parent-uid": pageUID, order: order + 1 },
-            block: { string: blocks[1].text.toString(), uid: thisBlock },
-          });
-        });
-      },
+      callback: () => fetchESVPassage(token),
     });
-
-    // Smart Block Command Registration
-    const args = {
-      text: "ESV",
-      help: "Import passages from ESV.org",
-      handler: (context) => fetchESV,
-    };
-
-    if (window.roamjs?.extension?.smartblocks) {
-      window.roamjs.extension.smartblocks.registerCommand(args);
-    } else {
-      document.body.addEventListener(
-        `roamjs:smartblocks:loaded`,
-        () =>
-          window.roamjs?.extension.smartblocks &&
-          window.roamjs.extension.smartblocks.registerCommand(args)
-      );
-    }
-
-    async function fetchESV(uid) {
-      var key, token;
-      breakme: {
-        if (!extensionAPI.settings.get("esv-org-api-key")) {
-          key = "token";
-          sendConfigAlert(key);
-          break breakme;
-        } else {
-          token = extensionAPI.settings.get("esv-org-api-key");
-        }
-
-        const pageId = window.roamAlphaAPI.pull("[*]", [":block/uid", uid])?.[
-          ":block/page"
-        ]?.[":db/id"];
-
-        const pageTitle = pageId
-          ? window.roamAlphaAPI.pull("[:node/title]", pageId)?.[":node/title"]
-          : window.roamAlphaAPI.pull("[:node/title]", [
-              ":block/uid",
-              await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid(),
-            ])?.[":node/title"];
-
-        // var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + pageTitle + "&origin=*";
-        var url = `${API_URL}?include-short-copyright=false&q=${pageTitle}`;
-
-        return fetch(url, {
-          headers: {
-            Authorization: `Token ${token}`,
-          },
-        })
-          .then((response) => response.json())
-          .then((pageID) => {
-            return !pageID
-              ? [{ text: "No items selected!" }]
-              : (() => {
-                  const getPassage = new Promise((resolve) => {
-                    fetch(url, {
-                      headers: {
-                        Authorization: `Token ${token}`,
-                      },
-                    })
-                      .then((r) => r.json())
-                      .then((data) => {
-                        console.log("data: " + JSON.stringify(data));
-
-                        var chapter = data.canonical;
-                        let passage = data["passages"][0];
-
-                        var regVerses = /\[(\d+)\]/g;
-                        let passages = passage.replace(
-                          regVerses,
-                          `\n\n` + `***${chapter}:` + `\$1:**`
-                        );
-                        navigator.clipboard.writeText(passages);
-                        var extractResults = { chapter, passages };
-                        resolve(extractResults);
-                      });
-                  });
-
-                  return Promise.allSettled([getPassage]).then(
-                    async (results) => {
-                      console.log("results: " + JSON.stringify(results));
-                      return [
-                        {
-                          text: "" + results[0].value.passages + "",
-                          children: [
-                            // { text: "" + results[0].value.passages + "" },
-                            // { text: "" + results[1].value + "" },
-                          ],
-                        },
-                        // {
-                        //     text: "" + results[0].value.cURL + "",
-                        // },
-                      ];
-                    }
-                  );
-                })();
-          });
-      }
-    }
   },
 
   onunload: () => {
@@ -283,7 +42,108 @@ export default {
     });
 
     if (window.roamjs?.extension?.smartblocks) {
-      window.roamjs.extension.smartblocks.unregisterCommand("ESV");
+      window.roamjs.extension.smartblocks.unregisterCommand("ESV Bible");
     }
   },
 };
+
+async function focusedBlockText(uid) {
+  let query = `[:find ?string
+                      :where [?b :block/uid "${uid}"]
+                             [?b :block/string ?string]]`;
+  return await window.roamAlphaAPI.q(query);
+}
+
+function fetchESVPassage(token) {
+  const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+
+  if (uid == undefined) {
+    alert("Please make sure to focus a block before searching ESV");
+    return;
+  }
+
+  let query = `[:find ?string
+                :where [?b :block/uid "${uid}"]
+                        [?b :block/string ?string]]`;
+  let focusedBlockText = window.roamAlphaAPI.q(query);
+
+  const pageId = window.roamAlphaAPI.pull("[*]", [":block/uid", uid])?.[
+    ":block/page"
+  ]?.[":db/id"];
+
+  // grab title if focused block text is empty
+  if (focusedBlockText == "") {
+    focusedBlockText = pageId
+      ? window.roamAlphaAPI.pull("[:node/title]", pageId)?.[":node/title"]
+      : window.roamAlphaAPI.pull("[:node/title]", [
+          ":block/uid",
+          window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid(),
+        ])?.[":node/title"];
+  }
+
+  // console.log("searchText: " + focusedBlockText);
+
+  fetchESV(focusedBlockText, token).then(async (blocks) => {
+    await window.roamAlphaAPI.updateBlock({
+      block: { uid: uid, string: blocks[0].text.toString(), open: true },
+    });
+
+    for (var i = 0; i < blocks[0].children.length; i++) {
+      var thisBlock = window.roamAlphaAPI.util.generateUID();
+      await window.roamAlphaAPI.createBlock({
+        location: { "parent-uid": uid, order: i + 1 },
+        block: {
+          string: blocks[0].children[i].text.toString(),
+          uid: thisBlock,
+        },
+      });
+    }
+  });
+}
+
+async function fetchESV(searchText, token) {
+  // console.log("fetchESV");
+
+  const API_URL = `https://api.esv.org/v3/passage/text/`;
+  var url = `${API_URL}?include-short-copyright=false&q=${searchText}`;
+
+  return fetch(url, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log("data: " + JSON.stringify(data));
+
+      var book_chapter = data.canonical.replace(/^(.*):(.*)$/g, `\$1`);
+
+      // replace verses to include book & chapter
+      let passages = data["passages"][0].replace(
+        /\[(\d+)\]/g,
+        `\n\n` + `**${book_chapter}:` + `\$1:**`
+      );
+
+      // copy to clipboard
+      navigator.clipboard.writeText(passages);
+
+      let children = passages
+        .split(`\n\n`)
+        .filter((el) => {
+          // remove any empty lines for cleaner roam bulletted list
+          return el.trim() != "";
+        })
+        .filter((el) => {
+          // the fetched json data includes a redundant passage so we remove the text
+          return el != searchText;
+        })
+        .map((verse) => ({ text: `${verse}` }));
+
+      return [
+        {
+          text: searchText,
+          children: children,
+        },
+      ];
+    });
+}
